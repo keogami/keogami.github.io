dd9e84ee8b8588234d398496c8bc8cf71663b17f
keogami <41939011+keogami@users.noreply.github.com>
2020-11-10 04:38:22 +0530

    finalized the retry mechanism

--- shooter/index.js
+++ shooter/index.js
@@ -15,9 +15,6 @@ const screen = new Screen($.querySelector("canvas#main"))
 screen.Resize(innerWidth, innerHeight)
 screen.SetBackground("#07020b")
 
-const player = new Player(screen.origin, 25, "#a80707", new Vector2(8, 8))
-screen.AddComponent("player", player)
-
 const reticle = new Reticle(screen.origin, "blue", 15)
 screen.AddComponent("reticle", reticle)
 
@@ -31,10 +28,12 @@ function spawnEnemies() {
   if (enemies._components.size > 5) {
     return
   }
-  enemies.Add(new Enemy(screen.RandomCoord({outside: true}), "brown", 35, new Vector2(0, 0), enemies))
+  enemies.Add(new Enemy(screen.RandomCoord({ outside: true }), "brown", 35, new Vector2(0, 0), enemies))
 }
+let stopSpawning = null
 
 function shootProjectiles() {
+  const player = screen.GetComponent("player")
   const vel = Vector2.Slope(player.coord, reticle.coord).Scale(16)
   projectiles.Add(new Projectile(player.coord, "pink", 8, vel, 3))
 }
@@ -57,21 +56,12 @@ const printScore = (val) => {
 }
 game.onScoreUpdate = printScore
 
-function frame({ timestamp, elapsedTime}) {
+function frame({ timestamp, elapsedTime }) {
   // Update and draw the screen
   screen.Update({ game, keys, mouse, elapsedTime, timestamp, screen })
   screen.Draw()
   if (keys.escape || game.state === Game.STATE_END) {
-    const animTime = 1000
-    setTimeout(() => {
-      setTimeout(() => {
-        scoreboard.classList.add("center")
-      }, animTime * 0.85)
-      requestAnimationFrame(screen.ClearAnimation(player.coord, "#07020b45", animTime))
-    }, 100)
-
-    setInterval(printScore, 100, game.score) // to prevent people from changing score manually
-    return
+    endGame()
   }
 
   // if debug data is required, calculate it and print it
@@ -85,17 +75,49 @@ function frame({ timestamp, elapsedTime}) {
     info.Set("Elapsed", `${elapsedTime.value}ms`)
     putInfo(info.String("\n"))
   }
- 
+
+}
+
+let scorePrintingIntervalHandle = null
+
+function endGame() {
+  stopSpawning()
+  removeEventListener("click", shootProjectiles)
+
+  const animTime = 1000
+  setTimeout(() => {
+    setTimeout(() => {
+      scoreboard.classList.add("center")
+    }, animTime * 0.85)
+    setTimeout(() => {
+      const it = $.querySelector("div.game-control")
+      it.classList.remove("clicked")
+      it.addEventListener("click", startGame)
+      // [Todo] incorrectly coupled with animTime, instead of scoreboard's animation time
+      // use transitionEnd on scoreboard as a trigger(?)
+    }, animTime * 2.2)
+    const player = screen.GetComponent("player")
+    requestAnimationFrame(screen.ClearAnimation(player.coord, "#07020b45", animTime))
+    scorePrintingIntervalHandle = setInterval(() => printScore(game.score), 400) // to prevent people from changing score manually
+  }, 100)
 }
 
 function startGame() {
+  scoreboard.classList.remove("center")
   this.removeEventListener("click", startGame)
   this.classList.add("clicked")
+  scorePrintingIntervalHandle ?? clearInterval(scorePrintingIntervalHandle)
+
+  screen.AddComponent("player", new Player(screen.origin, 25, "#a80707", new Vector2(8, 8)))
+  screen.GetComponent("projectiles").Clear()
+  screen.GetComponent("enemies").Clear()
+  game.score = 0
 
   setTimeout(() => {
     game.Run(frame)
     addEventListener("click", shootProjectiles)
-    setInterval(spawnEnemies, 1000)
+    const id = setInterval(spawnEnemies, 1000)
+    stopSpawning = () => clearInterval(id)
   }, 500)
 }
 
