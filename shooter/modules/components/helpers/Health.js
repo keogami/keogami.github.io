dd9973e189c51529610a4e68c39c5d3940f08d20
keogami <41939011+keogami@users.noreply.github.com>
2020-11-10 03:29:47 +0530

    the value of health can now be set in ratio to max

--- shooter/modules/components/helpers/Health.js
+++ shooter/modules/components/helpers/Health.js
@@ -8,6 +8,10 @@ class Health {
     return this._current / this._max
   }
 
+  set value(val) {
+    this._current = this._max * val
+  }
+
   Add(delta) {
     this._current = Math.min(this._max, Math.max(0, this._current + delta))
     return this
