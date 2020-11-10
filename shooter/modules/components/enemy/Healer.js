b70528db8cacd7ca2ce60a7474fc0d11347bd7cc
keogami <41939011+keogami@users.noreply.github.com>
2020-11-10 19:15:02 +0530

    Healers now add less HP

--- shooter/modules/components/enemy/Healer.js
+++ shooter/modules/components/enemy/Healer.js
@@ -2,7 +2,7 @@ import { Enemy } from "./Enemy.js"
 
 class Healer extends Enemy {
   constructor(coord, compSet) {
-    super(coord, "green", 25, 8, 2, -20, 5, compSet)
+    super(coord, "green", 25, 8, 2, -5, 5, compSet)
   }
 }
 
