e2ff771387e746fb1da3025af0b6288307d5fb5a
keogami <41939011+keogami@users.noreply.github.com>
2020-11-29 04:32:57 +0530

    Added a Class abstraction

---
+++ egraph/modules/Class.js
@@ -0,0 +1,20 @@
+import { Interval } from './Interval.js'
+
+class Class extends Interval {
+  constructor(lower, upper) {
+    super(lower, upper)
+    this.box = []
+  }
+
+  Record(val) {
+    if (!this.Accepts(val)) {
+      return false
+    }
+    this.box.push(val)
+    return true
+  }
+
+  get length() {
+    return this.box.length
+  }
+}
