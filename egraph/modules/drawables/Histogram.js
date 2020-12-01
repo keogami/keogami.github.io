240b7a0f46ce24637ebaf850db106781521ece82
keogami <41939011+keogami@users.noreply.github.com>
2020-12-01 23:27:28 +0530

    Histograms now extends the Drawable class

--- egraph/modules/drawables/Histogram.js
+++ egraph/modules/drawables/Histogram.js
@@ -1,26 +1,8 @@
-class Histogram {
+import { Drawable } from './Drawable.js'
+
+class Histogram extends Drawable {
   constructor() {
-    this._for = null
-    this._fixtures = {
-      x: 0, y: 0,
-    }
-    this._color = 'white'
-  }
-
-  For(freqDist) {
-    this._for = freqDist
-    return this
-  }
-
-  Fixtures(x, y) {
-    this._fixtures.x = x
-    this._fixtures.y = y
-    return this
-  }
-
-  Color(c) {
-    this._color = c
-    return this
+    super()
   }
 
   DrawOn(chart) {
