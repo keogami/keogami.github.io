f64558c16b08f9ff8ae90e6f3fa9f8c0219c77ca
keogami <41939011+keogami@users.noreply.github.com>
2021-02-14 23:22:46 +0530

    Got the explosion animation working properly

--- shooter/modules/components/Mine.js
+++ shooter/modules/components/Mine.js
@@ -33,14 +33,15 @@ class Mine {
     this._staticColor = this.color.toString()
 
     this._trigTime = null
-    this._trigFadeTime = 100 // ms
+    this._trigRatio = 0.0
+    this._trigFadeTime = 200 // ms
 
     this._cooked = 0.0
 
     this._state = new State(STATE_COOKING, mineStateTransitions)
   }
 
-  Update({ timestamp, screen}) {
+  Update({ timestamp, screen }) {
     if (this._state.Is(STATE_COOKING)) {
       const dt = (timestamp - this.setTime)
       this._cooked = Math.min(1.0, dt / this.cookTime)
@@ -54,53 +55,57 @@ class Mine {
         if (Coord.Dist(this.coord, enemy.coord) <= this.range) {
           this._state.Shift(STATE_TRIGGERED)
           this._trigTime = timestamp
-          console.log("triggered")
           break
         }
       }
     }
 
     if (this._state.Is(STATE_TRIGGERED)) {
-      const trigRatio = Math.min(1.0, (timestamp - this._trigTime) / this._trigFadeTime)
-      if (trigRatio === 1.0) {
+      this._trigRatio = Math.min(1.0, (timestamp - this._trigTime) / this._trigFadeTime)
+      this.color.alpha = 1.0 - this._trigRatio
+      this._staticColor = this.color.toString()
+      if (this._trigRatio === 1.0) {
         this.compSet.Remove(this)
         return
       }
       for (let enemy of screen.GetComponent("enemies").All()) {
         const dist = Coord.Dist(this.coord, enemy.coord)
         if (dist <= this.range) {
-          const damage = calculateDamage(this.damage, dist, trigRatio)
+          const damage = calculateDamage(this.damage, dist, this._trigRatio)
           enemy.Hit({ damage })
-          console.log({ damage, trigRatio, dist})
         }
       }
     }
   }
 
   Draw({ ctx }) {
-    if (!this._state.Is(STATE_TRIGGERED)) {
-      ctx.save()
+    ctx.save()
 
-      ctx.strokeStyle = ctx.fillStyle = ctx.shadowColor = this._staticColor
-      ctx.shadowBlur = this.size / 4
-      ctx.lineWidth = 3
+    ctx.strokeStyle = ctx.fillStyle = ctx.shadowColor = this._staticColor
+    ctx.shadowBlur = this.size / 4
+    ctx.lineWidth = 3
 
-      ctx.beginPath()
-      ctx.rect(this.coord.x - this.size/2, this.coord.y - this.size/2, this.size, this.size)
-      ctx.fill()
-      ctx.closePath()
+    ctx.beginPath()
+    ctx.rect(this.coord.x - this.size/2, this.coord.y - this.size/2, this.size, this.size)
+    ctx.fill()
+    ctx.closePath()
 
-      ctx.beginPath()
-      ctx.arc(this.coord.x, this.coord.y, this.size + 2, 0, 2 * Math.PI * this._cooked)
-      ctx.stroke()
-      ctx.closePath()
+    ctx.beginPath()
+    ctx.arc(this.coord.x, this.coord.y, this.size + 2, 0, 2 * Math.PI * this._cooked)
+    ctx.stroke()
+    ctx.closePath()
 
-      ctx.beginPath()
-      ctx.setLineDash([10, 5, 5, 5])
-      ctx.arc(this.coord.x, this.coord.y, this.range, 0, 2 * Math.PI)
-      ctx.stroke()
-      ctx.restore()
-    }
+    ctx.beginPath()
+    ctx.setLineDash([10, 5, 5, 5])
+    ctx.arc(this.coord.x, this.coord.y, this.range, 0, 2 * Math.PI)
+    ctx.stroke()
+    ctx.closePath()
+
+    ctx.beginPath()
+    ctx.arc(this.coord.x, this.coord.y, this.range * this._trigRatio, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.closePath()
+    ctx.restore()
   }
 }
 
